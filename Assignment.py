5188aeed84adcf45debf5b83c9edf5e7a553c1d0
AnasBarghouthi <anasbarghouthi1@gmail.com>
2025-10-22 23:41:41 +0300

    all code

--- Assignment.py
+++ Assignment.py
@@ -1,7 +1,7 @@
 import matplotlib.pyplot as plt
 import seaborn as sns
 import pandas as pd
-from IPython.display import display
+from sklearn.preprocessing import StandardScaler
 
 # Load dataset
 #step 1
@@ -21,10 +21,11 @@ print("Missing values before handling:\n", missing)
 
 # Example: Fill numeric columns with mean, categorical with mode
 for col in df.columns:
-    if df[col].dtype == 'float64' or df[col].dtype == 'int64':
-        df[col].fillna(df[col].mean(), inplace=True)
+    if df[col].dtype == 'float64' or df[col].dtype == 'int64':   #?we use mean here since it better for numerical value
+        df[col] = df[col].fillna(df[col].mean())
     else:
-        df[col].fillna(df[col].mode()[0], inplace=True)
+        df[col] = df[col].fillna(df[col].mode()[0])    #?we use mode here since it is not a numerical value
+
 
 # Verify
 print("\nMissing values after handling:\n", df.isnull().sum())
@@ -55,13 +56,55 @@ print(df.describe())
 
 
 
-scaler = StandardScaler()
+scaler = StandardScaler() #z-score
 scaled_cols = ['Age', 'Income', 'Tenure', 'SupportCalls']
 
 df[scaled_cols] = scaler.fit_transform(df[scaled_cols])
 
 print("Scaled data preview:")
-display(df.head())
+print(df.head())
+
+
+for col in numeric_features:
+    plt.figure(figsize=(5, 3))
+    plt.hist(x=df[col],color='skyblue', edgecolor='black')
+    plt.grid(True)
+    plt.title(f"histogram {col}")
+    plt.ylabel("Frequency")
+    plt.show()
+
+
+
+categorical_features =  ['Gender' , 'ProductType' , 'ChurnStatus' ] 
+
+for col in categorical_features:
+    plt.figure(figsize=(5, 3))
+    counts = df[col].value_counts()
+    plt.bar(counts.index, counts.values, color='red', edgecolor='black')
+    plt.title(f"bar {col}")
+    plt.ylabel("Frequency")
+    plt.show()
+
+
+for col in numeric_features :
+    plt.scatter(x=df['ChurnStatus'],y=df[col])
+    plt.show()
+
+
+corr_matrix = df[numeric_features].corr()
+print(corr_matrix)
+
+
+plt.scatter(x=df['Age'],y=df['Age'])
+plt.show()
+
+
+
+
+
+
+
+
 
 
 
