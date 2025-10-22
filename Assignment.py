d01e4e51e963ed1ca1dbedc09f805aa2d778d276
AnasBarghouthi <anasbarghouthi1@gmail.com>
2025-10-22 13:38:38 +0300

    code

--- Assignment.py
+++ Assignment.py
@@ -1,8 +1,10 @@
 import matplotlib.pyplot as plt
 import seaborn as sns
 import pandas as pd
+from IPython.display import display
 
 # Load dataset
+#step 1
 df = pd.read_csv("customer_data.csv")
 
 # Quick peek
@@ -12,13 +14,24 @@ print(df.info())
 print("\n--- Describe ---")
 print(df.describe)   
 
-print ("---- null values ----")
-print(df.isnull().sum())
+#step2
+# Check missing values again
+missing = df.isnull().sum()
+print("Missing values before handling:\n", missing)
+
+# Example: Fill numeric columns with mean, categorical with mode
+for col in df.columns:
+    if df[col].dtype == 'float64' or df[col].dtype == 'int64':
+        df[col].fillna(df[col].mean(), inplace=True)
+    else:
+        df[col].fillna(df[col].mode()[0], inplace=True)
+
+# Verify
+print("\nMissing values after handling:\n", df.isnull().sum())
+
 
 
 #handling outliers
-
-
 numeric_features = ['Age', 'Income', 'Tenure', 'SupportCalls']
 
 for col in numeric_features:
@@ -38,6 +51,17 @@ for col in numeric_features:
 
 print("\n Data after removing outliers:")
 print(df.describe())
+
+
+
+
+scaler = StandardScaler()
+scaled_cols = ['Age', 'Income', 'Tenure', 'SupportCalls']
+
+df[scaled_cols] = scaler.fit_transform(df[scaled_cols])
+
+print("Scaled data preview:")
+display(df.head())
 
 
 
